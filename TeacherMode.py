70cdfc8b96db8ae532507e8c6d63822614fec5d5
beenbeanz <haileylan404@gmail.com>
2025-02-25 08:57:39 -0500

    Update TeacherMode.py

---
+++ TeacherMode.py
@@ -0,0 +1,18 @@
+"""class Flashcard: 
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+
+chem = Flashcard("What is the only metal that is a liquid?", "Mercury")
+print(chem.question)"""
+
+class Person: 
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
