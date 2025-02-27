be7a3e55aa3d30886fcfd9b3443f27a160ebc16c
beenbeanz <haileylan404@gmail.com>
2025-02-27 09:25:00 -0500

    Update VoronoiVillages.py

--- VoronoiVillages.py
+++ VoronoiVillages.py
@@ -1,5 +1,13 @@
 def VoronoiVillages(villages):
-    villages
-    for i in villages:
+    neighborhoodSizes = []
+    villages.sort()
+    villages.remove(villages[0])
+    villages.remove(villages[len(villages) - 1])
+
+    for i in range(len(villages) - 1):
+        neighborhoodSize = villages[i+1] - villages[i]
+        neighborhoodSizes.append(neighborhoodSize)
         
+    print(villages)     
+    print(min(neighborhoodSizes))
 VoronoiVillages([5, 16, 0, 10, 4, 15])
