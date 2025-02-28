670e618b4763ed3a0de3df08946ecf3a2e82ee2a
beenbeanz <haileylan404@gmail.com>
2025-02-28 09:26:22 -0500

    Update TeacherMode.py

--- TeacherMode.py
+++ TeacherMode.py
@@ -2,17 +2,16 @@ import json
 
 flashcardsDict = {}
 
-inputQ = input("Input the question: ")
-inputA = input("Input the answer: ")
-
 class Flashcard: 
     def __init__(self, question, answer):
         self.question = question
         self.answer = answer
-        
+
+inputQ = input("Input the question: ")
+inputA = input("Input the answer: ")
+
 card =  Flashcard(inputQ, inputA)
 flashcardsDict[card.question] = card.answer
-
 
 with open('Flashcards.json', 'w') as json_file:
     json.dump(flashcardsDict, json_file, indent=4)
