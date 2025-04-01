ad83787a3e7d314ebaf8c7bc3dadb56089b7e50c
beenbeanz <haileylan404@gmail.com>
2025-04-01 09:26:57 -0400

    Update StudentMode.py

--- StudentMode.py
+++ StudentMode.py
@@ -6,5 +6,13 @@ try:
 except FileNotFoundError:
     cards_data = []
 
+score = 0
 for i in range(len(cards_data)):
-    print(cards_data[i]["image"])
+    print(cards_data[i]["question"])
+    ans = input('Your answer:')
+    if(ans == cards_data[i]["answer"]):
+        print("You're correct!")
+        score += 1
+    else:
+        print("That is so wrong are you stupid.")
+print(f"Your final score is {score}")
